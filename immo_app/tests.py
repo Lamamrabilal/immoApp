6f075eac77f61762ceb39ab3dd459fe31c9003e8
Lamamra Bilal <62879884+Lamamrabilal@users.noreply.github.com>
2023-04-18 23:56:46 +0200

    création de test unitaire de la classe locataire

--- immo_app/tests.py
+++ immo_app/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
 
-# Create your tests here.
+
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Locataire
+
+class TestLocataireView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('create_locataire')
+        self.data = {
+            'nom': 'Doe',
+            'prenom': 'John',
+            'adresse': '123 Main St',
+            'telephone': '555-1234'
+        }
+
+    def test_locataire_page_returns_200(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_locataire_form_submission_creates_new_object(self):
+        response = self.client.post(self.url, self.data)
+        self.assertEqual(response.status_code, 302)
+        locataires = Locataire.objects.all()
+        self.assertEqual(locataires.count(), 1)
+        locataire = locataires.first()
+        self.assertEqual(locataire.nom, 'Doe')
+        self.assertEqual(locataire.prenom, 'John')
+        self.assertEqual(locataire.adresse, '123 Main St')
+        self.assertEqual(locataire.telephone, '555-1234')
