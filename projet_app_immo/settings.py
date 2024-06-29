8463005e407e40bab4a71812ef319b42e698b114
Bilal Lamamra <bilal.lamamra@yahoo.fr>
2024-06-30 01:32:07 +0200

    mise à jour du fichier settings

--- projet_app_immo/settings.py
+++ projet_app_immo/settings.py
@@ -115,4 +115,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (BASE_DIR 
+STATICFILES_DIRS = (BASE_DIR / 'immo_app/static',)  # Ensure the tuple is properly closed
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
